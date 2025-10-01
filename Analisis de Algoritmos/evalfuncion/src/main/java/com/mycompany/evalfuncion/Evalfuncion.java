d1180895cbaf289f463a52aff782d0b94ec3ca56
Ahg3L <mgarciag2109@alumno.ipn.mx>
2025-09-30 20:21:52 -0600

    algoritmos

---
+++ Analisis de Algoritmos/evalfuncion/src/main/java/com/mycompany/evalfuncion/Evalfuncion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.evalfuncion;
+
+/**
+ *
+ * @author Ahg3L
+ */
+public class Evalfuncion {
+    public static void main(String[] args) {
+        evaluarfuncion wind = new evaluarfuncion();
+        wind.setVisible(true);
+    }
+}
